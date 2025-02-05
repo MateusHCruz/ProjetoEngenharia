78d7a389cd2006e72448eb99cbf780efd41f4e86
Mateus Chaves <mateushenrique0111@icloud.com>
2025-02-05 09:56:23 -0300

    Att

---
+++ menuMobile.js
@@ -0,0 +1,20 @@
+
+    let line1 = document.querySelector('.line-1');
+    let line2 = document.querySelector('.line-2');
+    let menu = document.querySelector('#menu-burguer'); 
+    let navAberta = document.querySelector('#navopen')
+    let body = document.querySelector('body')
+
+    console.log('line1:', line1);
+    console.log('line2:', line2);
+    console.log('menu:', menu);
+
+    
+        menu.addEventListener("click", () => {  
+            line1.classList.toggle('ativo1');
+            line2.classList.toggle('ativo2');
+            navAberta.classList.toggle('navAbertaAtiva');  
+            
+            body.classList.toggle('noscroll')
+        });
+    
